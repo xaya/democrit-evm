58ec48e631dda7e7dc72b7481988ae94cfb58ec6
Daniel Kraft <d@domob.eu>
2023-03-30 16:48:07 +0200

    Remove deployment config for truffle.
    This repository is not meant to deploy contracts, so we can get rid
    of the networks and deployment config.

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,3 @@
-var HDWalletProvider = require ("@truffle/hdwallet-provider");
-
-const Web3 = require ("web3");
-const web3 = new Web3 ();
-
 module.exports = {
   compilers: {
     solc: {
@@ -15,35 +10,6 @@ module.exports = {
       }
     }
   },
-  networks: {
-    mumbai: {
-      provider: function ()
-        {
-          return new HDWalletProvider ({
-            privateKeys: [process.env.PRIVKEY],
-            providerOrUrl: "https://rpc.ankr.com/polygon_mumbai"
-          });
-        },
-      network_id: 80001,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      maxFeePerGas: web3.utils.toWei ('50', 'gwei'),
-      maxPriorityFeePerGas: web3.utils.toWei ('35', 'gwei')
-    },
-    polygon: {
-      provider: function ()
-        {
-          return new HDWalletProvider ({
-            privateKeys: [process.env.PRIVKEY],
-            providerOrUrl: "https://polygon-rpc.com/"
-          });
-        },
-      network_id: 137,
-      confirmations: 2,
-      timeoutBlocks: 200,
-      maxFeePerGas: web3.utils.toWei ('100', 'gwei'),
-      maxPriorityFeePerGas: web3.utils.toWei ('35', 'gwei')
-    },
-  },
+  networks: {},
   plugins: ["solidity-coverage"]
 };
