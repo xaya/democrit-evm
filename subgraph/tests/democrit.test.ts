afa11a01c1b910c8c4ffcdbfc1004b6c7c985ad2
Daniel Kraft <d@domob.eu>
2023-03-31 15:51:45 +0200

    Unit tests for subgraph.
    This adds matchstick tests for the subgraph, verifying the Trade,
    Vault and SellOrder entities are correctly created and updated
    from contract events.

---
+++ subgraph/tests/democrit.test.ts
@@ -0,0 +1,117 @@
+import {
+  assert,
+  afterAll,
+  clearStore,
+  describe,
+  test,
+} from "matchstick-as/assembly/index"
+
+import {
+  testTrade,
+  testVaultCreated,
+  testVaultChanged,
+  testVaultEmptied,
+  testSellOrderCreated,
+  testSellOrderUpdated,
+  testSellOrderRemoved,
+} from "./utils"
+
+afterAll (clearStore)
+
+test ("Trades", () => {
+  testTrade (123, "gold", 10, 500, "seller", "buyer")
+  testTrade (456, "silver", 100, 20, "seller", "buyer")
+
+  assert.fieldEquals ("Trade", "1", "timestamp", "123")
+  assert.fieldEquals ("Trade", "1", "asset", "gold")
+  assert.fieldEquals ("Trade", "1", "amount", "10")
+  assert.fieldEquals ("Trade", "1", "sats", "500")
+  assert.fieldEquals ("Trade", "1", "price", "50")
+  assert.fieldEquals ("Trade", "1", "seller", "seller")
+  assert.fieldEquals ("Trade", "1", "buyer", "buyer")
+
+  assert.fieldEquals ("Trade", "2", "timestamp", "456")
+  assert.fieldEquals ("Trade", "2", "asset", "silver")
+  assert.fieldEquals ("Trade", "2", "amount", "100")
+  assert.fieldEquals ("Trade", "2", "sats", "20")
+  assert.fieldEquals ("Trade", "2", "price", "0.2")
+})
+
+describe ("Vaults", () => {
+  test ("Creation", () => {
+    testVaultCreated (42, "domob", "gold", 100)
+    testVaultCreated (50, "andy", "silver", 20)
+
+    assert.fieldEquals ("Vault", "42", "founder", "domob")
+    assert.fieldEquals ("Vault", "42", "asset", "gold")
+    assert.fieldEquals ("Vault", "42", "balance", "100")
+
+    assert.fieldEquals ("Vault", "50", "founder", "andy")
+    assert.fieldEquals ("Vault", "50", "asset", "silver")
+    assert.fieldEquals ("Vault", "50", "balance", "20")
+  })
+
+  test ("Update", () => {
+    testVaultCreated (42, "domob", "gold", 100)
+    testVaultChanged (42, 50)
+
+    assert.fieldEquals ("Vault", "42", "asset", "gold")
+    assert.fieldEquals ("Vault", "42", "balance", "50")
+  })
+
+  test ("Emptied", () => {
+    testVaultCreated (42, "domob", "gold", 100)
+    testVaultCreated (50, "domob", "silver", 20)
+
+    testVaultChanged (42, 0)
+    testVaultEmptied (42)
+
+    assert.notInStore ("Vault", "42")
+    assert.fieldEquals ("Vault", "50", "asset", "silver")
+    assert.fieldEquals ("Vault", "50", "balance", "20")
+  })
+})
+
+describe ("SellOrders", () => {
+  test ("Creation", () => {
+    testSellOrderCreated (10, 110, "seller", "gold", 10, 200)
+    testSellOrderCreated (20, 120, "seller", "silver", 100, 50)
+
+    assert.fieldEquals ("SellOrder", "10", "seller", "seller")
+    assert.fieldEquals ("SellOrder", "10", "asset", "gold")
+    assert.fieldEquals ("SellOrder", "10", "amount", "10")
+    assert.fieldEquals ("SellOrder", "10", "totalSats", "200")
+    assert.fieldEquals ("SellOrder", "10", "price", "20")
+
+    assert.fieldEquals ("SellOrder", "20", "asset", "silver")
+    assert.fieldEquals ("SellOrder", "20", "amount", "100")
+    assert.fieldEquals ("SellOrder", "20", "totalSats", "50")
+    assert.fieldEquals ("SellOrder", "20", "price", "0.5")
+  })
+
+  test ("Link to vault", () => {
+    testVaultCreated (101, "domob", "gold", 10)
+    testSellOrderCreated (1, 101, "domob", "gold", 10, 50)
+
+    assert.fieldEquals ("Vault", "101", "sellOrder", "1")
+    assert.fieldEquals ("SellOrder", "1", "vault", "101")
+  })
+
+  test ("Update", () => {
+    testSellOrderCreated (1, 100, "seller", "gold", 10, 5)
+    testSellOrderUpdated (1, 5, 1)
+
+    assert.fieldEquals ("SellOrder", "1", "amount", "5")
+    assert.fieldEquals ("SellOrder", "1", "totalSats", "1")
+    assert.fieldEquals ("SellOrder", "1", "price", "0.2")
+  })
+
+  test ("Removal", () => {
+    testSellOrderCreated (1, 100, "seller", "gold", 10, 5)
+    testSellOrderCreated (2, 200, "seler", "silver", 100, 10)
+    testSellOrderRemoved (2)
+
+    assert.fieldEquals ("SellOrder", "1", "asset", "gold")
+    assert.notInStore ("SellOrder", "2")
+  })
+})
---
+++ subgraph/tests/utils.ts
@@ -0,0 +1,138 @@
+import { newMockEvent } from "matchstick-as/assembly/index"
+import { ethereum } from "@graphprotocol/graph-ts"
+
+import {
+  Trade as TradeEvent,
+  VaultCreated as VaultCreatedEvent,
+  VaultChanged as VaultChangedEvent,
+  VaultEmptied as VaultEmptiedEvent,
+  SellOrderCreated as SellOrderCreatedEvent,
+  SellOrderUpdated as SellOrderUpdatedEvent,
+  SellOrderRemoved as SellOrderRemovedEvent,
+} from "../generated/Democrit/Democrit"
+
+import {
+  handleTrade,
+  handleVaultCreated,
+  handleVaultChanged,
+  handleVaultEmptied,
+  handleSellOrderCreated,
+  handleSellOrderUpdated,
+  handleSellOrderRemoved,
+} from "../src/democrit"
+
+import { BigInt } from "@graphprotocol/graph-ts"
+
+/* ************************************************************************** */
+
+export function testTrade (time: i32, asset: string, amount: i32, sats: i32,
+                           seller: string, buyer: string): void
+{
+  let t = changetype<TradeEvent> (newMockEvent ())
+  t.block.timestamp = BigInt.fromI32 (time)
+  t.parameters = new Array ()
+  t.parameters.push (
+      new ethereum.EventParam ("asset", ethereum.Value.fromString (asset)))
+  t.parameters.push (
+      new ethereum.EventParam ("amount", ethereum.Value.fromI32 (amount)))
+  t.parameters.push (
+      new ethereum.EventParam ("sats", ethereum.Value.fromI32 (sats)))
+  t.parameters.push (
+      new ethereum.EventParam ("seller", ethereum.Value.fromString (seller)))
+  t.parameters.push (
+      new ethereum.EventParam ("buyer", ethereum.Value.fromString (buyer)))
+  handleTrade (t)
+}
+
+/* ************************************************************************** */
+
+export function testVaultCreated (vaultId: i32, founder: string,
+                                  asset: string, initialBalance: i32): void
+{
+  let v = changetype<VaultCreatedEvent> (newMockEvent ())
+  v.parameters = new Array ();
+  v.parameters.push (
+      new ethereum.EventParam ("controller", ethereum.Value.fromString ("foo")))
+  v.parameters.push (
+      new ethereum.EventParam ("id", ethereum.Value.fromI32 (vaultId)))
+  v.parameters.push (
+      new ethereum.EventParam ("founder", ethereum.Value.fromString (founder)))
+  v.parameters.push (
+      new ethereum.EventParam ("asset", ethereum.Value.fromString (asset)))
+  v.parameters.push (
+      new ethereum.EventParam ("initialBalance",
+                               ethereum.Value.fromI32 (initialBalance)))
+  handleVaultCreated (v)
+}
+
+export function testVaultChanged (vaultId: i32, balance: i32): void
+{
+  let v = changetype<VaultChangedEvent> (newMockEvent ())
+  v.parameters = new Array ();
+  v.parameters.push (
+      new ethereum.EventParam ("controller", ethereum.Value.fromString ("foo")))
+  v.parameters.push (
+      new ethereum.EventParam ("id", ethereum.Value.fromI32 (vaultId)))
+  v.parameters.push (
+      new ethereum.EventParam ("asset", ethereum.Value.fromString ("ignored")))
+  v.parameters.push (
+      new ethereum.EventParam ("balance", ethereum.Value.fromI32 (balance)))
+  handleVaultChanged (v)
+}
+
+export function testVaultEmptied (vaultId: i32): void
+{
+  let v = changetype<VaultEmptiedEvent> (newMockEvent ())
+  v.parameters = new Array ();
+  v.parameters.push (
+      new ethereum.EventParam ("controller", ethereum.Value.fromString ("foo")))
+  v.parameters.push (
+      new ethereum.EventParam ("id", ethereum.Value.fromI32 (vaultId)))
+  handleVaultEmptied (v)
+}
+
+/* ************************************************************************** */
+
+export function testSellOrderCreated (orderId: i32, vaultId: i32,
+                                      seller: string, asset: string,
+                                      amount: i32, sats: i32): void
+{
+  let o = changetype<SellOrderCreatedEvent> (newMockEvent ())
+  o.parameters = new Array ();
+  o.parameters.push (
+      new ethereum.EventParam ("orderId", ethereum.Value.fromI32 (orderId)))
+  o.parameters.push (
+      new ethereum.EventParam ("vaultId", ethereum.Value.fromI32 (vaultId)))
+  o.parameters.push (
+      new ethereum.EventParam ("seller", ethereum.Value.fromString (seller)))
+  o.parameters.push (
+      new ethereum.EventParam ("asset", ethereum.Value.fromString (asset)))
+  o.parameters.push (
+      new ethereum.EventParam ("amount", ethereum.Value.fromI32 (amount)))
+  o.parameters.push (
+      new ethereum.EventParam ("totalSats", ethereum.Value.fromI32 (sats)))
+  handleSellOrderCreated (o);
+}
+
+export function testSellOrderUpdated (orderId: i32,
+                                      amount: i32, sats: i32): void
+{
+  let o = changetype<SellOrderUpdatedEvent> (newMockEvent ())
+  o.parameters = new Array ();
+  o.parameters.push (
+      new ethereum.EventParam ("orderId", ethereum.Value.fromI32 (orderId)))
+  o.parameters.push (
+      new ethereum.EventParam ("amount", ethereum.Value.fromI32 (amount)))
+  o.parameters.push (
+      new ethereum.EventParam ("totalSats", ethereum.Value.fromI32 (sats)))
+  handleSellOrderUpdated (o);
+}
+
+export function testSellOrderRemoved (orderId: i32,): void
+{
+  let o = changetype<SellOrderRemovedEvent> (newMockEvent ())
+  o.parameters = new Array ();
+  o.parameters.push (
+      new ethereum.EventParam ("orderId", ethereum.Value.fromI32 (orderId)))
+  handleSellOrderRemoved (o);
+}
